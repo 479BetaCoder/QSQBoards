6d18896cc3aadc3878162dc5c69d54f239965164
bhavyaharidas <haridas.b@husky.neu.edu>
2020-04-09 17:44:11 -0400

    Corrected overview route ref

--- webapp/src/app/app-routing/routes.ts
+++ webapp/src/app/app-routing/routes.ts
@@ -3,12 +3,12 @@ import {LoginComponent} from '../components/login/login.component';
 import {RegisterComponent} from '../components/register/register.component';
 import {HomeComponent} from '../components/home/home.component';
 import {UserProfileComponent} from '../components/user-profile/user-profile.component';
-import {OverViewComponent} from '../components/project/overview.component';
+import {OverviewComponent} from '../components/project/overview/overview.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'home', component: HomeComponent },
   { path: '',   redirectTo: '', pathMatch: 'full' },
-  { path: 'overview', component: OverViewComponent}
+  { path: 'overview', component: OverviewComponent}
 ];
