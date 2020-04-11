a099b98c57d7de437a9e9e809e19a45ea893aecd
Dileep Kumar Reddy Sirasani <sirasani.d@husky.neu.edu>
2020-04-11 02:55:42 -0400

    Modified Card UI and added avatar for projects based on first letter

--- webapp/src/app/components/home/home.component.ts
+++ webapp/src/app/components/home/home.component.ts
@@ -40,4 +40,10 @@ export class HomeComponent implements OnInit {
 
     this.projectDialog.open(ProjectDialogComponent, dialogConfig);
 }
+
+getRandomColor() {
+  var color = Math.floor(0x1000000 * Math.random()).toString(16);
+  return '#' + ('000000' + color).slice(-6);
+  }
+
 }
