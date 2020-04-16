10f5b1366e9b9dcfd43169962094dc983c9bbbd4
Sachin Dhumne <dhumne.s@husky.neu.edu>
2020-04-16 17:08:27 -0400

    Added table to show all the tasks

--- webapp/src/app/components/project/user-story-details/user-story-details.component.ts
+++ webapp/src/app/components/project/user-story-details/user-story-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-user-story-details',
@@ -6,10 +7,83 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-story-details.component.scss']
 })
 export class UserStoryDetailsComponent implements OnInit {
-
-  constructor() { }
+  updateStoryForm: FormGroup;
+  priorities = [
+    {value: 'low', viewValue: 'Low'},
+    {value: 'medium', viewValue: 'Medium'},
+    {value: 'high', viewValue: 'High'}];
+  taskTable: FormGroup;
+  control: FormArray;
+  mode: boolean;
+  touchedRows: any;
+  constructor(public fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.updateForm();
+    this.initiateForm();
+    this.touchedRows = [];
+    this.taskTable = this.fb.group({
+      tableRows: this.fb.array([])
+    });
+    this.addRow();
   }
 
+  updateForm() {
+    this.updateStoryForm = this.fb.group({
+      title: ['', [Validators.required]],
+      description: ['', [Validators.required]],
+      status: ['', [Validators.required, Validators.pattern]],
+      storyPoints: ['', [Validators.required]],
+      priority: ['', [Validators.required]],
+    });
+  }
+
+  initiateForm(): FormGroup {
+    return this.fb.group({
+      title: ['', [Validators.required]],
+      description: ['', [Validators.required]],
+      status: ['', [Validators.required, Validators.pattern]],
+      storyPoints: ['', [Validators.required]],
+      priority: ['', [Validators.required]],
+      assignee: ['', [Validators.required]],
+      isEditable: [true]
+    });
+  }
+
+  addRow() {
+    const control =  this.taskTable.get('tableRows') as FormArray;
+    control.push(this.initiateForm());
+  }
+
+  deleteRow(index: number) {
+    const control =  this.taskTable.get('tableRows') as FormArray;
+    control.removeAt(index);
+  }
+
+  editRow(group: FormGroup) {
+    group.get('isEditable').setValue(true);
+  }
+
+  doneRow(group: FormGroup) {
+    group.get('isEditable').setValue(false);
+  }
+
+  saveUserDetails() {
+    console.log(this.taskTable.value);
+  }
+
+  get getFormControls() {
+    const control = this.taskTable.get('tableRows') as FormArray;
+    return control;
+  }
+
+  submitForm() {
+    const control = this.taskTable.get('tableRows') as FormArray;
+    this.touchedRows = control.controls.filter(row => row.touched).map(row => row.value);
+    console.log(this.touchedRows);
+  }
+
+  toggleTheme() {
+    this.mode = !this.mode;
+  }
 }
