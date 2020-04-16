aa37a86dc85e003f94f3c1a46e4052d1f4078410
Sachin Dhumne <dhumne.s@husky.neu.edu>
2020-04-16 17:09:12 -0400

    Added directive for draggable

---
+++ webapp/src/app/directives/directive-column.directive.spec.ts
@@ -0,0 +1,8 @@
+import { DirectiveColumnDirective } from './directive-column.directive';
+
+describe('DirectiveColumnDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DirectiveColumnDirective();
+    expect(directive).toBeTruthy();
+  });
+});
